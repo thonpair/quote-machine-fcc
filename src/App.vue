359612c8ea9eddab15cf717fb54f78c73e28db30
thonpair <emmanuel@hemer.it>
2021-02-12 23:07:59 +0100

    projet ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuoteBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuoteBox from './components/QuoteBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    QuoteBox
   }
 }
 </script>
@@ -21,8 +20,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
+
 </style>
---
+++ src/components/QuoteBox.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="quote-box">
+      <div id="quote">
+      <div id="text">
+          {{ quote }}
+      </div>
+      <div id="author">
+          {{ author }}
+      </div>
+      </div>
+      <div id="buttons">
+        <button id="new-quote" v-on:click="getQuote">New Quote</button>
+        <a href="https://twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet it !</a>
+      </div>
+  </div>
+</template>
+
+<script>
+import quotes from './../../static/quotes.json'
+export default {
+    data(){
+        return{
+            quote: String,
+            author: String
+        }
+    },
+    methods:{
+        getQuote(){
+            const random = Math.floor(Math.random() * Math.floor(quotes.length))
+            const randomQuote = quotes[random];
+            this.quote = randomQuote.quote;
+            this.author = randomQuote.author;
+        }
+    },
+    created(){
+        this.getQuote();
+    }
+}
+</script>
+
+<style>
+#quote-box{
+    width: 400px;
+    margin: auto;
+    border: 1px solid grey;
+    border-radius: 20px;
+    padding: 20px;
+}
+#quote{
+    height: 150px;
+}
+#author{
+    text-align: right;
+    font-style: italic;
+}
+
+#buttons{
+    display: flex;
+    justify-content: center
+}
+
+#new-quote, #tweet-quote{
+    margin: 0px 30px;
+}
+
+#tweet-quote{
+    padding-top: 5px;
+}
+</style>
